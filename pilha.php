fb522f9f194bcbd0163cdd840575215fd6114389
Tiago <tiagosantos__@live.com>
2022-04-18 10:58:26 -0400

    pegando multiplas excecoes

--- pilha.php
+++ pilha.php
@@ -3,13 +3,22 @@
 function funcao1()
 {
     echo "Entrei na função 1" . PHP_EOL;
-    funcao2();
-    echo "Saindo da função 1" . PHP_EOL;
+    try {
+        funcao2();
+        echo "Saindo da função 1" . PHP_EOL;
+    } catch (RuntimeException | DivisionByZeroError $erroOuExcecao) {
+        echo $erroOuExcecao->getMessage() . PHP_EOL;
+        echo $erroOuExcecao->getLine() . PHP_EOL;
+        echo $erroOuExcecao->getTraceAsString() . PHP_EOL;
+    }
 }
 
 function funcao2()
 {
     echo "Entrei na função 2" . PHP_EOL;
+    $divisao = intdiv(5, 0);
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = "valor";
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
